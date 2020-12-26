63a3e333ba9258b77553e2c805ae7d0161f4d8e9
mykytapavlov <mykytapavlov@users.noreply.github.com>
2020-12-26 22:34:38 +0200

    Changes url in data.js to reference markdown folder

--- docs/javascript/data.js
+++ docs/javascript/data.js
@@ -1,33 +1,10 @@
 const data = {
     "name": "python",
-    "url": "https://github.com/mykytapavlov/nerd-old/tree/master/python",
+    "url": "",
     "children": [
         {
-            "name": "core",
-            "url": "https://github.com/mykytapavlov/nerd-old/tree/master/python/core",
-            "children": [
-                {
-                    "name": "interpreter",
-                    "children": [
-                        {
-                            "name": "code object", 
-                            "url": "https://github.com/mykytapavlov/nerd-old/blob/master/python/core/closure.md",
-                        },
-                        {
-                            "name": "byte object",
-                            "url": "https://github.com/mykytapavlov/nerd-old/blob/master/python/core/closure.md",
-                        },
-                        {
-                            "name": "vm",
-                            "url": "https://github.com/mykytapavlov/nerd-old/blob/master/python/core/closure.md",
-                        },
-                        {
-                            "name": "cpython",
-                            "url": "https://github.com/mykytapavlov/nerd-old/blob/master/python/core/closure.md",
-                        }
-                    ]
-                },
-            ]
+            "name": "closure",
+            "url": "markdown/python/closure",
         },
     ]
 }
